63156a4aea4d8cf76bc6022e756f6978243b3db9
boram13 <bmenerja@helius-systems.com>
2023-10-09 11:23:47 +0200

    clickable left button

--- src/components/dashboard/dashboard.controller.js
+++ src/components/dashboard/dashboard.controller.js
@@ -1,4 +1,14 @@
-angular.module('dashboard', ['ngMaterial'])
 app.controller('dashboard', function ($scope) {
     $scope.title = 'Dashboard'
+    // insert methods
+    function switchSideBar(){
+
+        
+        let sidebarleft = this;
+
+        document.getElementByClassName('sidebar')[0].classList.add('showSideBar')
+         
+        }
+
 });
+
