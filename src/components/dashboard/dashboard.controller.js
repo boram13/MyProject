6cb106f35073c28d675ef4ec10801968bdeafb51
boram13 <bmenerja@helius-systems.com>
2023-10-11 10:22:19 +0200

    full scrreen height navbar and nav label tab

--- src/components/dashboard/dashboard.controller.js
+++ src/components/dashboard/dashboard.controller.js
@@ -1,17 +1,19 @@
 app.controller('dashboard', function ($scope) {
-    $scope.title = 'Dashboard'
+    $scope.title = '' //here we can put a title to our controller which will be poped out in the center of the app
 
     $scope.switchTab = function () {
 
         const sidebar = document.querySelector('.sidebar');
         sidebar.classList.toggle('fliph');
-        
+
 
         console.log('clicked')
     }
     return this
 });
 
+
+//me juery angularjs
 // $(document).ready(function(){
 //     $('.button-left').click(function(){
 //         $('.sidebar').toggleClass('fliph');
